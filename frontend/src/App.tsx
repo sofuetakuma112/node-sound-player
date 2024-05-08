2a7dc659bf28d2fe27dede93f93b1aef92a92ee9
sofuetakuma <kawahagi0620@gmail.com>
2024-05-08 12:58:59 +0900

    スタイリング修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,12 +12,12 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col p-4 sm:p-8">
-      <h1 className="text-4xl mb-4 sm:mb-8 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+    <div className="min-h-screen flex flex-col p-8 sm:p-16">
+      <h1 className="text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         Sound Palyer
       </h1>
       <div className="flex-1 flex flex-col justify-center items-center">
-        <Button onClick={handlePlaySound} className="w-[180px]">
+        <Button onClick={handlePlaySound} className="w-[180px] sm:w-[256px] sm:text-lg">
           音を再生する
         </Button>
       </div>

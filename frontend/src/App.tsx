805edbc3198a8008a717aaca532c9760375616b9
sofuetakuma <kawahagi0620@gmail.com>
2024-05-08 12:33:40 +0900

    スタイリング修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import axios from "axios"
+import axios from "axios";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const handlePlaySound = async () => {
@@ -11,9 +12,15 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Sound Player</h1>
-      <button onClick={handlePlaySound}>Play Sound</button>
+    <div className="min-h-screen flex flex-col p-4 sm:p-8">
+      <h1 className="text-4xl mb-4 sm:mb-8 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Sound Palyer
+      </h1>
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <Button onClick={handlePlaySound} className="w-[180px]">
+          音を再生する
+        </Button>
+      </div>
     </div>
   );
 }

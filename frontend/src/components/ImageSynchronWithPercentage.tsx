811d6e4de79e086970375fda2ea8fe4bae449ac7
sofuetakuma <kawahagi0620@gmail.com>
2024-05-10 08:34:43 +0900

    コメント部分を削除

--- frontend/src/components/ImageSynchronWithPercentage.tsx
+++ frontend/src/components/ImageSynchronWithPercentage.tsx
@@ -53,14 +53,4 @@ export const ImageSynchronWithPercentage = ({ imageSources, value }: Props) => {
       ))}
     </div>
   );
-
-  // return (
-  //   <div className={clsx("flex-1 flex justify-center bg-no-repeat", {
-  //       "bg-top": value === 100,
-  //       "bg-center": value !== 100
-  //   })} style={{
-  //       backgroundImage: `url(${getImageSource(value)})`
-  //   }}>
-  //   </div>
-  // );
 };
